a76ad7f25643a97b8646aedeff41f1e6e8e43c96
Robert Obi <robobi97@googlemail.com>
2021-02-13 19:04:59 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -60,11 +60,9 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  matrix_updated[row][column] = new_value
-  matrix
-  
   matrix[row][column] = new_value
   matrix
+  
   
   
   
